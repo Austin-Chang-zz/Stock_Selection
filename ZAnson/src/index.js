487fdc863f3f04f7f71e7f25fbaa44a0e3a5a0b3
Austin-Chang-zz <>
2025-10-24 09:17:44 +0800

    add express validation

--- ZAnson/src/index.js
+++ ZAnson/src/index.js
@@ -1,11 +1,36 @@
 import express, { request, response } from "express";
+import { query,validationResult,body} from "express-validator";
 
 const app = express();
 
-// 添加中間件(midware)來解析 JSON 請求體
+// 添加中間件(middleware)來解析 JSON 請求體
 app.use(express.json());
+const loggingMiddleware = (request, response, next) => {
+    console.log(`${request.method} - ${request.url}`);
+    next();
+};
+
+// app.use(loggingMiddleware);
+
+const resolveIndexByUserID = (request, response, next) => {
+    //define request format , all body by id
+    const { body, params: { id }, } = request;
+    const parsedId = parseInt(id);
+    //check the correct id as body index
+    if (isNaN(parsedId))
+        return response.sendStatus(400);  //bad request
+    const findUserIndex = mockUsers.findIndex((user) => user.id === parsedId);
+    //find the correct index and body
+    if (findUserIndex === -1)
+        return response.sendStatus(404); //not found
+    request.findUserIndex = findUserIndex; 
+    next();
+};
+
 // 添加中間件來解析 URL 編碼的請求體
 app.use(express.urlencoded({ extended: true }));
+
+
 
 const PORT = process.env.PORT || 3000;
 
@@ -24,87 +49,36 @@ const mockUsers = [
 //     response.send("Hello, World");
 // });
 
-// send JSON object response
-app.get("/", (request, response) => {
-    response.status(200).send({msg: "Hello"});
-});
-
-
-//post a new user body as feedback of request
-app.post("/api/users",
-    (request, response) => {
-        const { body } = request;  //request a body
-        //define a new user begin from the last user id + body
-        const newUser = { id: mockUsers[mockUsers.length - 1].id + 1, ...body }; 
-        mockUsers.push(newUser);
-        console.log(req.body);
-    return response.status(201).send(newUser);
+//send JSON object response
+app.get("/",
+    (request, response, next) => { 
+        console.log("Base URL 1");
+        next();
+    },
+    (request, response, next) => {
+        console.log("Base URL 2");
+        next();
+    },
+    (request, response, next) => {
+        console.log("Base URL 3");
+        next();
+    },
+    (request,response) =>{
+        response.status(200).send({msg: "Hello"});
     }
 );
-
-app.get("/api/users/:id", (request, response) => {
-    console.log(request.params);
-    const parsedId = parseInt(request.params.id);
-
-    //print out the typed id of URL on console
-    console.log(parsedId);
-    //print out the status 400 if not a number
-    if (isNaN(parsedId)) return response.status(400).send({ msg: "Bad Request. Invalid" });
-
-    const findUser = mockUsers.find((user) => user.id === parsedId);
-    if (!findUser) return response.sendStatus(404);
-    return response.send(findUser);
-
-});
-
-app.get("/api/products", (request, response) => {
-    response.send([{ id: 123, name: "chicken breast", price: 12.99 }]);
-});
-
-//put request
-app.put("/api/users/:id",
-    (request, response) => {
-        //define request format , all body by id
-        const {body,params: { id },} = request;
-        const parsedId = parseInt(id);
-        //check the correct id as body index
-        if (isNaN(parsedId))
-            return response.sendStatus(400);  //bad request
-        const findUserIndex = mockUsers.findIndex((user) => user.id === parsedId);
-        //find the correct index and body
-        if (findUserIndex === -1)
-            return response.sendStatus(404); //not found
-        mockUsers[findUserIndex] = { id: parsedId, ...body };
-
-    //send correct request body
-    return response.sendStatus(200); //OK
-    }
-);
-
-
-//patch request (only change some field of body)
-app.patch("/api/users/:id",
-    (request, response) => {
-        //define request format , all body by id
-        const { body, params: { id }, } = request;
-        const parsedId = parseInt(id);
-        //check the correct id as body index
-        if (isNaN(parsedId))    
-            return response.sendStatus(400);  //bad request
-        const findUserIndex = mockUsers.findIndex((user) => user.id === parsedId);
-        //find the correct index and body
-        if (findUserIndex === -1)
-            return response.sendStatus(404); //not found
-        mockUsers[findUserIndex] = { ...mockUsers[findUserIndex], ...body }
-        
-    return response.sendStatus(200);
-    }
-);
-
 
 //make route
-app.get("/api/users", (request, response) => {
-    console.log(request.query);
+app.get("/api/users", query("filter")
+    .isString()
+    .notEmpty()
+    .withMessage("Must not be empty")
+    .isLength({ min: 3, max: 10 })
+    .withMessage("Must be at least 3-10 characters"),
+    (request, response) => {
+        console.log(request["acceptedexpress - validator#contexts"]);
+    const result = validationResult(request);
+        console.log(result);
     const {
         query: { filter, value },
     } = request;
@@ -119,6 +93,82 @@ app.get("/api/users", (request, response) => {
 
     return response.send(mockUsers);
 });
+
+//declare the middleware, then middleware will be responsed after this instruction
+// app.use(loggingMiddleware); //---------important--------
+app.use(loggingMiddleware, (request, response, next) => {
+    console.log("Finished Logging...");
+    next();
+});
+
+//post a new user body as feedback of request
+app.post("/api/users",
+    body("username")
+        .notEmpty()
+        .withMessage("username can not be empty")
+        .isLength({ min: 5, max: 32 })
+        .withMessage("User name must be between 5-32 characters")
+        .isString()
+        .withMessage("Username must be a string"),
+    body("displayName").notEmpty(),
+    (request, response) => {
+        const result = validationResult(request);
+        console.log(result);
+        const { body } = request;  //request a body
+        //define a new user begin from the last user id + body
+        const newUser = { id: mockUsers[mockUsers.length - 1].id + 1, ...body }; 
+        mockUsers.push(newUser);
+        console.log(request.body);
+    return response.status(201).send(newUser);
+    }
+);
+
+app.get("/api/users/:id", resolveIndexByUserID,
+    (request, response) => {
+        const { findUserIndex } = request;
+        const findUser = mockUsers[findUserIndex];
+    if (!findUser) return response.sendStatus(404);
+    return response.send(findUser);
+
+});
+
+app.get("/api/products", (request, response) => {
+    response.send([{ id: 123, name: "chicken breast", price: 12.99 }]);
+});
+
+//put request
+app.put("/api/users/:id",resolveIndexByUserID,
+    (request, response) => {
+        //define request format , all body by id
+        const {body,findUserIndex} = request;
+        mockUsers[findUserIndex] = { id: mockUsers[findUserIndex].id, ...body };
+
+    //send correct request body
+    return response.sendStatus(200); //OK
+    }
+);
+
+
+//patch request (only change some field of body)
+app.patch("/api/users/:id",resolveIndexByUserID,
+    (request, response) => {
+        //define request format , all body by id
+        const { body, findUserIndex } = request;
+        mockUsers[findUserIndex] = { ...mockUsers[findUserIndex], ...body }
+        
+    return response.sendStatus(200);
+    }
+);
+
+//delete request
+app.delete("/api/users/:id", resolveIndexByUserID,
+    (request, response) => {
+        //define request format , all body by id
+        const { findUserIndex } = request;
+        mockUsers.splice(findUserIndex,1);
+        return response.sendStatus(200);
+    }
+);
 
 // app.listen(PORT, () => {
 //     console.log('running on Port ${PORT}');
